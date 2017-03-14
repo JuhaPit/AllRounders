f918c8f968a5d5034c6323a4bdba1f2283b9a4b0
Antti Ryökkynen <antti.ryokkynen@myy.haaga-helia.fi>
2017-03-14 16:51:56 +0200

    Changed top 7 drivers endpoint => /top

--- src/main/java/com/fineline/web/ARController.java
+++ src/main/java/com/fineline/web/ARController.java
@@ -1,13 +1,8 @@
 package com.fineline.web;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.inject.Inject;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fineline.domain.Topten;
-import com.fineline.domain.WorkDay;
-import com.fineline.service.WorkDayDAO;
 import com.fineline.domain.WorkDay_GoogleSheets;
 import com.fineline.service.GoogleDataFetcher;
 
@@ -26,52 +19,36 @@ public class ARController {
 	/*
 	 * @Inject WorkDayDAO workdaydao;
 	 */
-	@RequestMapping(value = "/test", produces = "application/json", method = RequestMethod.GET)
-	public List<Topten> hello() throws IOException {
-		System.out.println("ARController.hello()");
-		// GoogleDataFetcher.getSheetsService();
-		// GoogleDataFetcher.authorize();
-		List<Topten> topten = GoogleDataFetcher.topten();
-
-		return topten;
-
+	@RequestMapping(value = "/top", produces = "application/json", method = RequestMethod.GET)
+	public List<Topten> topSeven() throws IOException {
+		List<Topten> topSeven = GoogleDataFetcher.topten();
+		return topSeven;
 	}
 
 	@RequestMapping(value = "/listall", produces = "application/json", method = RequestMethod.GET)
 	public List<WorkDay_GoogleSheets> listEverything() throws IOException {
-
-		// GoogleDataFetcher.getSheetsService();
-		// GoogleDataFetcher.authorize();
 		List<WorkDay_GoogleSheets> listall = GoogleDataFetcher.listAll();
 		return listall;
-
 	}
 
 	@RequestMapping(value = "/tophel", produces = "application/json", method = RequestMethod.GET)
 	public List<Topten> topHelsinki() throws IOException {
-
 		List<Topten> tophel = GoogleDataFetcher.tophel();
 		return tophel;
-
 	}
 
 	@RequestMapping(value = "/topvan", produces = "application/json", method = RequestMethod.GET)
 	public List<Topten> topVantaa() throws IOException {
-
 		List<Topten> topvan = GoogleDataFetcher.topvan();
 		System.out.println();
 		return topvan;
-
 	}
 
 	@RequestMapping(value = "/avg/{name}", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Topten> getAvgFromDriver(@PathVariable("name") String name)
 			throws IOException {
-
-		System.out.println(name);
 		List<Topten> avgDriver = GoogleDataFetcher.driverAvg(name);
-
 		return avgDriver;
 	}
 

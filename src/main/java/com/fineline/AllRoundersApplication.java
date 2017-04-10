6cb00fcbc243eef4a332ddb19cad457924668924
Antti Ryökkynen <antti.ryokkynen@myy.haaga-helia.fi>
2017-04-10 15:23:06 +0300

    Added version info

--- src/main/java/com/fineline/AllRoundersApplication.java
+++ src/main/java/com/fineline/AllRoundersApplication.java
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AllRoundersApplication {
 	
-	
-	
-
 	public static void main(String[] args) {
 		SpringApplication.run(AllRoundersApplication.class, args);
+		
 	}
 }

1c4997869463f0363e11bb34be78b842f5432270
Laurasvaara Inka <a1502992@myy.haaga-helia.fi>
2017-04-10 14:55:56 +0300

    gi# Please enter the commit message for your changes. Lines starting

--- src/main/java/com/fineline/web/ListingController.java
+++ src/main/java/com/fineline/web/ListingController.java
@@ -8,8 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ListingController {
 	@RequestMapping("/listing")
-	public String restapi() {
+	public String listing() {
 		return "listing";
 	}
 	
+	@RequestMapping("/mainpage")
+	public String mainpage() {
+		return "mainpage";
+	}	
+	
 }
+
